2a65e956ae9eccafdb1f12c3e342eb8f35da781a
deejayy <deejayy@deejayy.hu>
2022-02-08 20:55:28 +0100

    Basic version

--- mainu.dfm
+++ mainu.dfm
@@ -1,10 +1,10 @@
 object KeyForm: TKeyForm
-  Left = 824
-  Top = 403
-  Width = 285
-  Height = 90
+  Left = 818
+  Top = 235
+  Width = 475
+  Height = 379
   Caption = 'Clickety-click'
-  Color = clBtnFace
+  Color = 2629664
   Font.Charset = DEFAULT_CHARSET
   Font.Color = clWindowText
   Font.Height = -11
@@ -15,6 +15,30 @@ object KeyForm: TKeyForm
   Position = poScreenCenter
   OnCreate = FormCreate
   OnDestroy = FormDestroy
+  DesignSize = (
+    459
+    340)
   PixelsPerInch = 96
   TextHeight = 13
+  object ListBox1: TListBox
+    Left = 16
+    Top = 16
+    Width = 425
+    Height = 313
+    Anchors = [akLeft, akTop, akRight, akBottom]
+    BevelEdges = []
+    BevelInner = bvNone
+    BevelOuter = bvNone
+    BorderStyle = bsNone
+    Color = 2629664
+    Font.Charset = ANSI_CHARSET
+    Font.Color = clSilver
+    Font.Height = -21
+    Font.Name = 'Segoe UI'
+    Font.Style = []
+    ItemHeight = 30
+    ParentFont = False
+    TabOrder = 0
+    OnClick = ListBox1Click
+  end
 end
--- mainu.pas
+++ mainu.pas
@@ -4,49 +4,58 @@ interface
 
 uses
   Windows, Messages, SysUtils, Variants, Classes, Graphics, Controls, Forms,
-  Dialogs, StdCtrls, WaveMix;
+  Dialogs, StdCtrls, WaveMix, Inifiles;
 
 const
   KeyEvent = WM_USER + 1;
 
 type
-  TKeyForm = class(TForm)
+  TKeyForm = class (TForm)
+    ListBox1: TListBox;
     procedure FormCreate(Sender: TObject);
     procedure FormDestroy(Sender: TObject);
+    procedure ListBox1Click(Sender: TObject);
   private
-    procedure KeyEventHandler(var Msg : TMessage); message KeyEvent;
+    procedure KeyEventHandler(var Msg: TMessage); message KeyEvent;
+    procedure LoadSounds(folder: string);
   end;
 
 var
   KeyForm: TKeyForm;
   WaveMix: TWaveMix;
   WaveFileKeyDown, WaveFileKeyUp: PMixWave;
+  DownSounds: array of PMixWave;
+  UpSounds: array of PMixWave;
+
+function ScanDir: TStrings;
 
 implementation
 
 
-function SetKeyHook : Longint; stdcall; external 'Key.dll';
-function DelKeyHook : Longint; stdcall; external 'Key.dll';
+function SetKeyHook: Longint; stdcall; external 'Key.dll';
+function DelKeyHook: Longint; stdcall; external 'Key.dll';
 
 {$R *.dfm}
 
-var i: integer = 0; 
+var i: integer = 0;
 
 procedure TKeyForm.FormCreate(Sender: TObject);
 begin
- WaveMix := TWaveMix.Create();
- WaveFileKeyDown := WaveMix.OpenFromFile('keydown.wav');
- WaveFileKeyUp := WaveMix.OpenFromFile('keyup.wav');
- WaveMix.Channels := $FF;
- WaveMix.Activated := true;
- SetKeyHook;
+  WaveMix := TWaveMix.Create();
+  WaveFileKeyDown := WaveMix.OpenFromFile('keydown.wav');
+  WaveFileKeyUp := WaveMix.OpenFromFile('keyup.wav');
+  WaveMix.Channels := $FF;
+  WaveMix.Activated := true;
+  Listbox1.Items := ScanDir;
+  KeyForm.LoadSounds('cherry-mx-red');
+  SetKeyHook;
 end;
 
 procedure TKeyForm.FormDestroy(Sender: TObject);
 begin
- DelKeyHook;
- WaveMix.Activated := false;
- WaveMix.Destroy();
+  DelKeyHook;
+  WaveMix.Activated := false;
+  WaveMix.Destroy();
 end;
 
 procedure TKeyForm.KeyEventHandler(var Msg: TMessage);
@@ -54,13 +63,75 @@ var KeyState: integer;
 begin
   inc(i);
   KeyState := Msg.LParam shr 30;
-  if (KeyState = 0) then begin
+  if (KeyState = 0) then
+  begin
+    WaveFileKeyDown := DownSounds[Random(Length(DownSounds))];
     WaveMix.Play(i mod 8, WaveFileKeyDown, nil, WMIX_USELRUCHANNEL or WMIX_HIGHPRIORITY, 0);
   end;
 
-  if (KeyState = 3) then begin
+  if (KeyState = 3) then
+  begin
+    WaveFileKeyUp := UpSounds[Random(Length(UpSounds))];
     WaveMix.Play(i mod 8, WaveFileKeyUp, nil, WMIX_USELRUCHANNEL or WMIX_HIGHPRIORITY, 0);
   end;
+end;
+
+function ScanDir(): TStrings;
+var
+  Path: String;
+  SR: TSearchRec;
+  DirList: TStrings;
+begin
+  Path := '.\sound\';
+  DirList := TStringList.Create;
+  try
+    if FindFirst(Path + '*.*', faAnyFile, SR) = 0 then
+    begin
+      repeat
+        if (SR.Name <> '.') and (SR.Name <> '..') then
+        begin DirList.Add(SR.Name) end;
+      until FindNext(SR) <> 0;
+      FindClose(SR);
+    end;
+
+    result := dirlist;
+
+  finally
+  end;
+
+end;
+
+procedure TKeyForm.LoadSounds(folder: string);
+var ini: TIniFile;
+  list: TStrings;
+  i: integer;
+begin
+  list := TStringList.Create;
+  ini := TInifile.Create('.\sound\' + folder + '\config.ini');
+  ini.ReadSection('down', list);
+  if list.count > 0 then
+  begin
+    SetLength(DownSounds, list.count);
+    for i := 0 to list.Count - 1 do
+    begin
+      DownSounds[i] := WaveMix.OpenFromFile('.\sound\' + folder + '\' + ini.ReadString('down', list[i], ''));
+    end;
+  end;
+
+  ini.ReadSection('up', list);
+  if list.count > 0 then
+  begin
+    SetLength(UpSounds, list.count);
+    for i := 0 to list.Count - 1 do
+    begin
+      UpSounds[i] := WaveMix.OpenFromFile('.\sound\' + folder + '\' + ini.ReadString('up', list[i], ''));
+    end;
+  end;
+end;
+
+procedure TKeyForm.ListBox1Click(Sender: TObject);
+begin
+  keyform.loadsounds(listbox1.Items[listbox1.ItemIndex]);
 end;
 
 end.
